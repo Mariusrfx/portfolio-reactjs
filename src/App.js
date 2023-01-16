7d4976140c40ef8c1490af37a6eeddc946adb01f
Mariusrfx <mariusrfx@gmail.com>
2023-01-16 21:32:51 +0100

    Navbar responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar";
 
 
 
 function App() {
   return (
     <div className="App">
-        <h1 className='text-3xl font-bold'>Hello</h1>
+        <Navbar/>
     </div>
   );
 }
