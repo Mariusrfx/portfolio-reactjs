46fe42feb35595e1b74cf0d603cd7757dcf692df
Mariusrfx <mariusrfx@gmail.com>
2023-02-16 23:52:44 +0100

    v1.4.5.1 fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { FaLinkedin,FaInstagram,FaGithub } from "react-icons/fa";
 function Footer() {
     const {t}=useTranslation();
   return (
-    <div className="bg-[#0a192f] pb-12 pt-5">
+    <div className="bg-[#0a192f] pb-10 pt-10">
       {" "}
       <div className="w-full h-full text-center text-gray-300  flex justify-center items-center p-4">
         <p>{t('footdesc')}</p>
